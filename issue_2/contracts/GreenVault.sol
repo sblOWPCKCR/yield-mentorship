a11344f18439148fa8dce61c08c06590153707dd
Owleksiy owPckCr <sblOWPCKCR@protonmail.com>
2021-08-13 16:43:22 -0700

    SMTChecker experiments
    + `yarn check` runs SMTChecker on your contract
    + preprocessor step
    
    To do anything useful, the checker relies on assert() statements in
    code. While some of them might be OK to have in production code, most of
    them use stack (because you need to explicitly store state sometimes).
    
    To aid with that, there's a new preprocessor step that comments out all
    lines with "#nonprod" suffix. Example:
    https://rinkeby.etherscan.io/address/0xc0bBBd6ed9092Ed978CAbDAdC74cb021b8330d41#code
    , look at withdraw() function at the end
    
    `yarn check` is the only command that DOES NOT run the preprocessor and
    has full access to all asserts().
    
    It is not clear if the checked is useful, but it's worth playing around
    with

--- issue_2/contracts/GreenVault.sol
+++ issue_2/contracts/GreenVault.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.4;
 
 import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 // error InsufficientBalance();
 // ^ can't use this b/c waffle doesn't support custom errors
@@ -10,7 +11,7 @@ import "@yield-protocol/utils-v2/contracts/token/IERC20.sol";
 @title GreenToken
 @notice Just a standard ERC20 token that can be minted by anybody
  */
-contract GreenVault {
+contract GreenVault is ReentrancyGuard {
     /// @notice emitted on deposit
     event EventDeposit(address user, address token, uint256 amount);
     /// @notice emitted on withdrawal
@@ -26,7 +27,7 @@ contract GreenVault {
     @param token token address
     @param amount amount of tokens to deposit
      */
-    function deposit(address token, uint256 amount) public {
+    function deposit(address token, uint256 amount) public nonReentrant {
         IERC20(token).transferFrom(msg.sender, address(this), amount);
 
         deposits[msg.sender][token] += amount;
@@ -40,12 +41,16 @@ contract GreenVault {
     @param amount amount of tokens to withdraw
      */
 
-    function withdraw(address token, uint256 amount) public {
+    function withdraw(address token, uint256 amount) public nonReentrant {
         require(deposits[msg.sender][token] >= amount, "InsufficientBalance");
 
+        uint256 balance = deposits[msg.sender][token]; // #noprod
         deposits[msg.sender][token] -= amount;
 
         IERC20(token).transfer(msg.sender, amount);
+
+        assert(deposits[msg.sender][token] == balance - amount); // #noprod
+
         emit EventWithdraw(msg.sender, token, amount);
     }
 }
