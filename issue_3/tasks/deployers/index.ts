f48b6fcef5452fa1f78140357693975db18cabe8
sblOWPCKCR <sblOWPCKCR@protonmail.com>
2021-08-14 08:17:03 -0700

    Issue 3: Wrapped vault (#4)
    * Issue 3: Wrapped vault
    
    GreenToken https://kovan.etherscan.io/address/0xD76C8a919e23D05691EcdEaE662D792137EAC746#code
    GreenVault https://kovan.etherscan.io/address/0xc0bBBd6ed9092Ed978CAbDAdC74cb021b8330d41#code

--- issue_3/tasks/deployers/index.ts
+++ issue_3/tasks/deployers/index.ts
@@ -3,10 +3,22 @@ import { TaskArguments } from "hardhat/types";
 
 task("deploy")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    for (const contract_name of ["GreenToken", "GreenVault"]) {
+    const addresses: {[name: string]: string} = {};
+    const initializers : {[name: string]: () => Iterable<any>} = {
+      "GreenToken": () => {return []},
+      "GreenVault": () => {return [addresses["GreenToken"]];}
+    }
+
+    for (const contract_name in initializers) {
+      console.log("Deploying ", contract_name);
+
       const factory = await ethers.getContractFactory(contract_name);
-      const contract = await factory.deploy();
+      const args = initializers
+      [contract_name]();
+
+      const contract = await factory.deploy(...args);
       await contract.deployed();
+      addresses[contract_name] = contract.address;
       console.log(contract_name, " deployed to: ", contract.address);
     }
   });
--- issue_3/test/TestGreenVault.ts
+++ issue_3/test/TestGreenVault.ts
@@ -34,11 +34,9 @@ describe("Unit tests", function () {
       this.gtoken = <GreenToken>await deployContract(this.signers.admin,
         await hre.artifacts.readArtifact("GreenToken"), []);
 
-      this.another_token = <GreenToken>await deployContract(this.signers.admin,
-        await hre.artifacts.readArtifact("GreenToken"), []);
-
       this.gvault = (<GreenVault>await deployContract(this.signers.admin,
-        await hre.artifacts.readArtifact("GreenVault"), [])).connect(this.signers.user1);
+        await hre.artifacts.readArtifact("GreenVault"), [this.gtoken.address]))
+        .connect(this.signers.user1);
 
       for (const user of [this.signers.user1, this.signers.user2]) {
         await this.gtoken.connect(user).mint(user.address, starting_balance);
@@ -46,93 +44,75 @@ describe("Unit tests", function () {
     });
 
     it("starts empty", async function () {
-      expect(await this.gvault.deposits(this.signers.user1.address, this.gtoken.address))
+      expect(await this.gvault.balanceOf(this.signers.user1.address))
         .to.be.equal(0);
     });
 
     it("doesn't accepts transfers without approvals", async function () {
-      await expect(this.gvault.deposit(this.gtoken.address, starting_balance))
+      await expect(this.gvault.mint(starting_balance))
         .to.be.reverted; // can't check for exact message since it's ERC20-generated and is not prescribed
     });
 
     it("doesn't accepts out-of-balance transfers", async function () {
       await this.gtoken.approve(this.gvault.address, starting_balance * 2);
 
-      await expect(this.gvault.deposit(this.gtoken.address, starting_balance * 2))
+      await expect(this.gvault.mint(starting_balance * 2))
         .to.be.reverted; // can't check for exact message since it's ERC20-generated and is not prescribed
     });
 
-    describe("transfers", function () {
+    describe("mints", function () {
       beforeEach(async function () {
         await this.gtoken.connect(this.signers.user1).approve(this.gvault.address, starting_balance);
       });
       it("change balance", async function () {
-        await expect(() => this.gvault.deposit(this.gtoken.address, starting_balance))
+        await expect(() => this.gvault.mint(starting_balance))
           .to.changeTokenBalances(this.gtoken, [this.signers.user1, this.gvault], [-starting_balance, starting_balance]);
 
-        expect(await this.gvault.deposits(this.signers.user1.address, this.gtoken.address))
+        expect(await this.gvault.balanceOf(this.signers.user1.address))
           .to.be.equal(starting_balance);
       });
 
       it("emit events", async function () {
-        await expect(this.gvault.deposit(this.gtoken.address, starting_balance))
-          .to.emit(this.gvault, "EventDeposit").withArgs(this.signers.user1.address, this.gtoken.address, starting_balance);
+        await expect(this.gvault.mint(starting_balance))
+          .to.emit(this.gvault, "EventMint").withArgs(this.signers.user1.address, this.gtoken.address, starting_balance);
       });
 
       it("don't change other user's balance", async function () {
-        await this.gvault.deposit(this.gtoken.address, starting_balance);
-
-        expect(await this.gvault
-            .connect(this.signers.user2)
-            .deposits(this.signers.user2.address, this.gtoken.address))
-          .to.be.equal(0);
+        await expect (() => this.gvault.mint(starting_balance))
+          .to.changeTokenBalance(this.gtoken, this.signers.user2, 0);
 
       });
-
-      it("don't change other token's balance", async function () {
-        await this.gvault.deposit(this.gtoken.address, starting_balance);
-
-        expect(await this.gvault.deposits(this.signers.user1.address, this.another_token.address))
-          .to.be.equal(0);
-
-      });
-
     });
 
-    describe("withdrawals", function () {
+    describe("burns", function () {
       beforeEach(async function () {
         await this.gtoken.connect(this.signers.user1).approve(this.gvault.address, starting_balance);
-        await this.gvault.deposit(this.gtoken.address, starting_balance);
+        await this.gvault.mint(starting_balance);
       });
 
       it("by different users are not permitted", async function () {
         await expect(this.gvault
-            .connect(this.signers.user2)
-            .withdraw(this.gtoken.address, 1))
-          .to.be.revertedWith("InsufficientBalance");
-      })
-
-      it("to different token are not permitted", async function () {
-        await expect(this.gvault.withdraw(this.another_token.address, 1))
-          .to.be.revertedWith("InsufficientBalance");
+          .connect(this.signers.user2)
+          .burn(1))
+          .to.be.revertedWith("ERC20: Insufficient balance");
       })
 
       it("with insufficient balance are not permitted ", async function () {
-        await expect(this.gvault.withdraw(this.gtoken.address, starting_balance * 2))
-          .to.be.revertedWith("InsufficientBalance");
+        await expect(this.gvault.burn(starting_balance * 2))
+          .to.be.revertedWith("ERC20: Insufficient balance");
       })
 
       it("change the balance", async function () {
-        await expect(() => this.gvault.withdraw(this.gtoken.address, starting_balance))
+        await expect(() => this.gvault.burn(starting_balance))
           .to.changeTokenBalances(this.gtoken, [this.signers.user1, this.gvault], [starting_balance, -starting_balance]);
 
-        expect(await this.gvault.deposits(this.signers.user1.address, this.gtoken.address))
+        expect(await this.gvault.balanceOf(this.signers.user1.address))
           .to.be.equal(0);
       })
 
       it("emit correct message", async function () {
-        await expect(this.gvault.withdraw(this.gtoken.address, starting_balance))
-          .to.emit(this.gvault, "EventWithdraw").withArgs(this.signers.user1.address, this.gtoken.address, starting_balance);
+        await expect(this.gvault.burn(starting_balance))
+          .to.emit(this.gvault, "EventBurn").withArgs(this.signers.user1.address, this.gtoken.address, starting_balance);
       })
 
     });
