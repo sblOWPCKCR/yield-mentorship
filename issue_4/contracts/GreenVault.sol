e8456f2875ba3658bce9361edd7a80e859f4adb8
sblOWPCKCR <sblOWPCKCR@protonmail.com>
2021-08-18 08:33:56 -0700

    Issue 4: Wrapped Vault with fractions (#6)
    * Issue 4: Wrapped Vault with fractions
    
    Same old GreenVault with 1 extra field: `rewardsFraction`. Represented
    as wad
    
    GreenToken: https://kovan.etherscan.io/address/0x9Bf598A28849594E6d864c337705C01eE6847E36#code
    GreenVault: https://kovan.etherscan.io/address/0x12792f1610D09237a0337Ee770be3229c0De84dF#code

--- issue_4/contracts/GreenVault.sol
+++ issue_4/contracts/GreenVault.sol
@@ -1,46 +1,78 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
+import "@yield-protocol/utils-v2/contracts/access/Ownable.sol";
 import "@yield-protocol/utils-v2/contracts/token/ERC20.sol";
 
-// error InsufficientBalance();
-// ^ can't use this b/c waffle doesn't support custom errors
+/**
+@dev Internal library that implements Fixed Point Math
+All numbers are WADs - fixed point numbers with decimals=18
+ */
+library FPMath {
+    uint256 internal constant WAD = 1e18;
+
+    /**
+    @notice Multiply 2 WADs
+    */
+    function wadMul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 ret = (a * b);
+        require(b == 0 || ret / b == a, "wadMul: overflow");
+        return ret / WAD;
+    }
+}
 
 /**
 @title GreenVault
 @notice A (single-token) vault that mints wrapped tokens
 in response to deposits
  */
-contract GreenVault is ERC20("GreenVaultToken", "OWL_GVT", 18) {
+contract GreenVault is ERC20("GreenVaultToken", "OWL_GVT", 18), Ownable {
     /**
     @notice the only token this vault supports
      */
-    IERC20 public theOnlySupportedToken;
+    IERC20Metadata public theOnlySupportedToken;
+
+    /**
+    Multiplier: for each token deposited we mint `exchangeRateWad` Vault Tokens
+
+    It's represented as a wad (a 18-digit fixed point number)
+     */
+    uint256 public exchangeRateWad;
 
     /// @notice emitted on deposit
-    event EventMint(address user, address token, uint256 amount);
+    event Minted(address user, address token, uint256 amount);
     /// @notice emitted on withdrawal
-    event EventBurn(address user, address token, uint256 amount);
+    event Burned(address user, address token, uint256 amount);
+
+    event ExchangeRateSet(uint256 exchangeRateWad);
 
     /**
     @param token address of the only token this vault will support
+    @param _exchangeRateWad exchange rate to start with
      */
-    constructor(IERC20 token) {
+    constructor(IERC20Metadata token, uint256 _exchangeRateWad) {
         theOnlySupportedToken = token;
+        setExchangeRate(_exchangeRateWad);
+    }
+
+    function setExchangeRate(uint256 _exchangeRateWad) public onlyOwner {
+        exchangeRateWad = _exchangeRateWad;
+        emit ExchangeRateSet(exchangeRateWad);
     }
 
     /**
     @notice Mint Vault token by depositing external token
     @param amount amount of tokens to deposit
      */
-    function mint(uint256 amount) public {
+    function deposit(uint256 amount) public {
         IERC20 token = theOnlySupportedToken;
 
-        _mint(msg.sender, amount);
+        uint256 exchangeAmount = FPMath.wadMul(amount, exchangeRateWad);
+        _mint(msg.sender, exchangeAmount);
 
         token.transferFrom(msg.sender, address(this), amount);
 
-        emit EventMint(msg.sender, address(token), amount);
+        emit Minted(msg.sender, address(token), exchangeAmount);
     }
 
     /**
@@ -48,13 +80,14 @@ contract GreenVault is ERC20("GreenVaultToken", "OWL_GVT", 18) {
     @param amount amount of tokens to withdraw
      */
 
-    function burn(uint256 amount) public {
+    function withdraw(uint256 amount) public {
         IERC20 token = theOnlySupportedToken;
 
-        _burn(msg.sender, amount);
+        uint256 exchangeAmount = FPMath.wadMul(amount, exchangeRateWad);
+        _burn(msg.sender, exchangeAmount);
 
         token.transfer(msg.sender, amount);
 
-        emit EventBurn(msg.sender, address(token), amount);
+        emit Burned(msg.sender, address(token), exchangeAmount);
     }
 }
