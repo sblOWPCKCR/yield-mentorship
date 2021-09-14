09671e68a300b3b77dca427bf6951492a1bf7964
Owleksiy owPckCr <sblOWPCKCR@protonmail.com>
2021-09-13 17:09:12 -0700

    comments

--- issue_6/contracts/GreenAMM.sol
+++ issue_6/contracts/GreenAMM.sol
@@ -24,52 +24,35 @@ library FPMath {
     }
 }
 
-/// @dev placeholder to keep all token-related data
-struct TokenData {
-    uint256 reserves;
-    // ended up not needing anything else
-}
-
 /**
 @notice 'Engine' of the standard xy=k AMM contract
  */
 abstract contract GreenAMMEngine is Ownable {
-    bool internal isInitialized;
+    using FPMath for uint256;
 
     /// @dev info about X token
-    TokenData public x;
+    uint256 public xReserves;
     /// @dev info about Y token
-    TokenData public y;
+    uint256 public yReserves;
 
     /// @dev K from X*Y=K
     /// IMIPORTANT: K has 36 decimals
     uint256 public k;
 
     event KChanged(uint256 newK);
-    event Inited(uint256 x, uint256 y);
+    event Initialized(uint256 x, uint256 y);
     event Minted(uint256 x, uint256 y);
     event Burned(uint256 x, uint256 y);
     event SoldX(uint256 x, uint256 y);
     event SoldY(uint256 x, uint256 y);
 
     modifier initialized() {
-        require(isInitialized, "not initialized");
+        require(xReserves > 0 || yReserves > 0, "not initialized");
         _;
     }
 
     modifier notInitialized() {
-        require(!isInitialized, "already initialized");
-        _;
-    }
-
-    /// @dev asserts that amountX/amountY have same ratio as existing reserves
-    modifier maintainsBalance(uint256 amountX, uint256 amountY) {
-        require(x.reserves != 0 && y.reserves != 0, "Empty pool");
-
-        // since decimals for X and Y are unchanged, don't bother computing *exact* value for x/y:
-        // x1 * dec_x / y1 * dec_y == x2 * dec_x / y2 * dec_y IMPLIES x1/y1 == x2/y2
-        // x1/y1 == x2/y2 IS EQUIVALENT to x1 * y2 == x2 * y1
-        require(x.reserves * amountY == y.reserves * amountX, "Unbalancing");
+        require(xReserves == 0 && yReserves == 0, "already initialized");
         _;
     }
 
@@ -83,78 +66,78 @@ abstract contract GreenAMMEngine is Ownable {
     constructor(uint8 xDecimals, uint8 yDecimals) {
         require(xDecimals == 18, "Only WAD-denominated tokens are supported");
         require(yDecimals == 18, "Only WAD-denominated tokens are supported");
-        x = TokenData(0);
-        y = TokenData(0);
+    }
+
+    /// @dev checks if amountX/amountY have same ratio as existing reserves
+    function maintainsBalance(uint256 amountX, uint256 amountY) internal view returns (bool) {
+        require(xReserves != 0 && yReserves != 0, "Empty pool");
+
+        // since decimals for X and Y are unchanged, don't bother computing *exact* value for x/y:
+        // x1 * dec_x / y1 * dec_y == x2 * dec_x / y2 * dec_y IMPLIES x1/y1 == x2/y2
+        // x1/y1 == x2/y2 IS EQUIVALENT to x1 * y2 == x2 * y1
+        return (xReserves * amountY == yReserves * amountX);
     }
 
     /// @dev recompute K based on new reserve numbers
-    function kRecompute() internal {
-        k = x.reserves * y.reserves; // use increased precision
+    function _kRecompute() internal {
+        k = xReserves * yReserves; // use increased precision
         emit KChanged(k);
     }
 
     /**
-    @notice 'State Chage' function for 'Init' method
+    @notice 'State Chage' function for 'init' method
     @param amountX X tokens to deposit
     @param amountY Y tokens to deposit
     @return new K value
      */
-    function stateChangeInit(uint256 amountX, uint256 amountY) internal notInitialized onlyOwner returns (uint256) {
+    function _init(uint256 amountX, uint256 amountY) internal notInitialized onlyOwner returns (uint256) {
         require(amountX != 0 && amountY != 0, "Can't create empty pool");
 
-        isInitialized = true;
+        xReserves += amountX;
+        yReserves += amountY;
 
-        x.reserves += amountX;
-        y.reserves += amountY;
-
-        kRecompute();
+        _kRecompute();
         return k;
     }
 
     /**
-    @notice 'State Chage' function for 'Mint' method
+    @notice 'State Chage' function for 'mint' method
     @param amountX X tokens to deposit
     @param amountY Y tokens to deposit
     @return rewardsFraction how many AMM tokens need to be issued (36-decimal fraction of total supply)
      */
-    function stateChangeMint(uint256 amountX, uint256 amountY)
-        internal
-        initialized
-        maintainsBalance(amountX, amountY)
-        returns (uint256 rewardsFraction)
-    {
-        rewardsFraction = FPMath.wadDiv(amountX, x.reserves);
+    function stateChangeMint(uint256 amountX, uint256 amountY) internal initialized returns (uint256 rewardsFraction) {
+        require(maintainsBalance(amountX, amountY), "Unbalancing");
 
-        x.reserves += amountX;
-        y.reserves += amountY;
+        rewardsFraction = amountX.wadDiv(xReserves);
 
-        kRecompute();
+        xReserves += amountX;
+        yReserves += amountY;
+
+        _kRecompute();
     }
 
     /**
-    @notice 'State Chage' function for 'Burn' method
+    @notice 'State Chage' function for 'burn' method
     @param amountX X tokens to burn
     @param amountY Y tokens to burn
     @return rewardsFraction how many AMM tokens need to be burned (36-decimal fraction of total supply)
      */
-    function stateChangeBurn(uint256 amountX, uint256 amountY)
-        internal
-        initialized
-        maintainsBalance(amountX, amountY)
-        returns (uint256 rewardsFraction)
-    {
-        rewardsFraction = FPMath.wadDiv(amountX, x.reserves);
+    function stateChangeBurn(uint256 amountX, uint256 amountY) internal initialized returns (uint256 rewardsFraction) {
+        require(maintainsBalance(amountX, amountY), "Unbalancing");
 
-        x.reserves -= amountX;
-        y.reserves -= amountY;
+        rewardsFraction = amountX.wadDiv(xReserves);
 
-        kRecompute();
+        xReserves -= amountX;
+        yReserves -= amountY;
+
+        _kRecompute();
 
         // BurnInteraction(amountX, amountY, rewardsAmount);
     }
 
     /**
-    @notice 'State Chage' function for 'SellX' method
+    @notice 'State Chage' function for 'sellX' method
     @param amountX X tokens to add to reserves
     @return how many Y tokens need to be returned to the user
      */
@@ -162,23 +145,23 @@ abstract contract GreenAMMEngine is Ownable {
         // (x + A) * (y - B) = k
         // y - B = k / (x + A)
         // B = y - k / (x + A)
-        uint256 amountY = y.reserves - k / (x.reserves + amountX);
-        x.reserves += amountX;
-        y.reserves -= amountY;
+        uint256 amountY = yReserves - k / (xReserves + amountX);
+        xReserves += amountX;
+        yReserves -= amountY;
 
         return amountY;
     }
 
     /**
-    @notice 'State Chage' function for 'SellY' method
+    @notice 'State Chage' function for 'sellY' method
     @param amountY Y tokens to add to reserves
     @return how many X tokens need to be returned to the user
      */
 
     function stateChangeSellY(uint256 amountY) internal initialized returns (uint256) {
-        uint256 amountX = x.reserves - k / (y.reserves + amountY);
-        x.reserves -= amountX;
-        y.reserves += amountY;
+        uint256 amountX = xReserves - k / (yReserves + amountY);
+        xReserves -= amountX;
+        yReserves += amountY;
 
         return amountX;
     }
@@ -188,43 +171,45 @@ abstract contract GreenAMMEngine is Ownable {
 @notice Standard k=xy AMM contract
  */
 contract GreenAMM is ERC20("GreenAMMToken", "OWL_GAMT", 36), GreenAMMEngine {
+    using FPMath for uint256;
+
     /// @dev X token
     IERC20Metadata internal xToken;
     /// @dev Y token
     IERC20Metadata internal yToken;
 
     constructor(IERC20Metadata _x, IERC20Metadata _y) GreenAMMEngine(_x.decimals(), _y.decimals()) {
-        require(address(_x) != address(_y), "bad pair");
+        require(address(_x) != address(_y), "tokens need to be different");
         xToken = _x;
         yToken = _y;
     }
 
     /**
     @notice Supply any amount of x and y to the contract
-    Mint k AMM tokens to sender. Can only be called once.
+    mint k AMM tokens to sender. Can only be called once.
 
     @param amountX X tokens to deposit
     @param amountY Y tokens to deposit
     */
-    function Init(uint256 amountX, uint256 amountY) public {
-        uint256 rewardsAmount = stateChangeInit(amountX, amountY);
+    function init(uint256 amountX, uint256 amountY) public {
+        uint256 rewardsAmount = _init(amountX, amountY);
         _mint(msg.sender, rewardsAmount);
 
         xToken.transferFrom(msg.sender, address(this), amountX);
         yToken.transferFrom(msg.sender, address(this), amountY);
 
-        emit Inited(amountX, amountY);
+        emit Initialized(amountX, amountY);
     }
 
     /**
     @notice x and y are supplied to the contract in the same proportion as the x and y balances of the contract
-    Mint AMM tokens to sender as the proportion of their deposit to the AMM reserves
+    mint AMM tokens to sender as the proportion of their deposit to the AMM reserves
 
     @param amountX X tokens to deposit
     @param amountY Y tokens to deposit
     */
-    function Mint(uint256 amountX, uint256 amountY) public {
-        uint256 rewardsAmount = FPMath.wadMul(stateChangeMint(amountX, amountY), _totalSupply);
+    function mint(uint256 amountX, uint256 amountY) public {
+        uint256 rewardsAmount = stateChangeMint(amountX, amountY).wadMul(_totalSupply);
 
         _mint(msg.sender, rewardsAmount);
 
@@ -241,8 +226,8 @@ contract GreenAMM is ERC20("GreenAMMToken", "OWL_GAMT", 36), GreenAMMEngine {
     @param amountX X tokens to burn
     @param amountY Y tokens to burn
     */
-    function Burn(uint256 amountX, uint256 amountY) public {
-        uint256 rewardsAmount = FPMath.wadMul(stateChangeBurn(amountX, amountY), _totalSupply);
+    function burn(uint256 amountX, uint256 amountY) public {
+        uint256 rewardsAmount = stateChangeBurn(amountX, amountY).wadMul(_totalSupply);
 
         _burn(msg.sender, rewardsAmount);
 
@@ -258,7 +243,7 @@ contract GreenAMM is ERC20("GreenAMMToken", "OWL_GAMT", 36), GreenAMMEngine {
 
     @param amountX X tokens the user sells
     */
-    function SellX(uint256 amountX) public {
+    function sellX(uint256 amountX) public {
         uint256 amountY = stateChangeSellX(amountX);
 
         xToken.transferFrom(msg.sender, address(this), amountX);
@@ -273,7 +258,7 @@ contract GreenAMM is ERC20("GreenAMMToken", "OWL_GAMT", 36), GreenAMMEngine {
 
     @param amountY Y tokens the user sells
     */
-    function SellY(uint256 amountY) public {
+    function sellY(uint256 amountY) public {
         uint256 amountX = stateChangeSellY(amountY);
 
         yToken.transferFrom(msg.sender, address(this), amountY);
@@ -289,16 +274,18 @@ contract GreenAMM is ERC20("GreenAMMToken", "OWL_GAMT", 36), GreenAMMEngine {
 Each invariant ends with 'revert' to workaround the fact that the Engine is stateful
  */
 contract GreenAMMEngineTest is GreenAMMEngine {
+    using FPMath for uint256;
+
     constructor(uint8 xDecimals, uint8 yDecimals) GreenAMMEngine(xDecimals, yDecimals) {}
 
-    /// @dev Init test: K is recomputed
+    /// @dev init test: K is recomputed
     function invInit(uint256 amountX, uint256 amountY) public {
-        uint256 tokens = stateChangeInit(amountX, amountY);
+        uint256 tokens = _init(amountX, amountY);
         assert(tokens == k);
         revert("inv");
     }
 
-    /// @dev Mint test: K is recomputed and tokens ratio stay the same
+    /// @dev mint test: K is recomputed and tokens ratio stay the same
     function invMint(
         uint256 x1,
         uint256 y1,
@@ -310,8 +297,8 @@ contract GreenAMMEngineTest is GreenAMMEngine {
         require(y1 != 0, "inv");
         require(y2 != 0, "inv");
 
-        uint256 supply1 = stateChangeInit(x1, y1);
-        uint256 supply2 = FPMath.wadMul(stateChangeMint(x2, y2), supply1);
+        uint256 supply1 = _init(x1, y1);
+        uint256 supply2 = stateChangeMint(x2, y2).wadMul(supply1);
         assert(x2 / x1 == supply2 / supply1);
         revert("inv");
     }
@@ -325,15 +312,15 @@ contract GreenAMMEngineTest is GreenAMMEngine {
         require(x1 != 0, "inv");
         require(y1 != 0, "inv");
 
-        stateChangeInit(x1, y1);
+        _init(x1, y1);
         uint256 oldK = k;
-        uint256 oldX = x.reserves;
+        uint256 oldX = xReserves;
         stateChangeSellX(sellAmount);
 
         assert(k == oldK);
-        assert(x.reserves == oldX + sellAmount);
+        assert(xReserves == oldX + sellAmount);
 
-        assert(x.reserves * y.reserves == k);
+        assert(xReserves * yReserves == k);
         revert("inv");
     }
 
@@ -346,15 +333,15 @@ contract GreenAMMEngineTest is GreenAMMEngine {
         require(x1 != 0, "inv");
         require(y1 != 0, "inv");
 
-        stateChangeInit(x1, y1);
+        _init(x1, y1);
         uint256 oldK = k;
-        uint256 oldY = y.reserves;
+        uint256 oldY = yReserves;
         stateChangeSellY(sellAmount);
 
         assert(k == oldK);
-        assert(y.reserves == oldY + sellAmount);
+        assert(yReserves == oldY + sellAmount);
 
-        assert(x.reserves * y.reserves == oldK);
+        assert(xReserves * yReserves == oldK);
         revert("inv");
     }
 }
--- issue_6/test/TestGreenAMM.ts
+++ issue_6/test/TestGreenAMM.ts
@@ -56,7 +56,7 @@ describe("Unit tests", function () {
         .to.be.equal(0);
     });
 
-    describe("Init", function () {
+    describe("init", function () {
       beforeEach(async function () {
         // re-create gAMM before each 'init' test
         this.gAMM = (<GreenAMM>await deployContract(this.admin,
@@ -68,35 +68,35 @@ describe("Unit tests", function () {
       });
 
       it("can be done by the owner", async function () {
-        this.gAMM.Init(1, 1);
+        this.gAMM.init(1, 1);
       })
 
       it("can't be done by a non-owner", async function () {
-        await expect(this.gAMM.connect(this.user1).Init(1, 1))
+        await expect(this.gAMM.connect(this.user1).init(1, 1))
           .to.be.revertedWith("Ownable: caller is not the owner");
       })
 
       it("can be done only once", async function () {
-        this.gAMM.Init(1, 1);
-        await expect(this.gAMM.Init(1, 1))
+        this.gAMM.init(1, 1);
+        await expect(this.gAMM.init(1, 1))
           .to.be.revertedWith("already initialized");
       })
 
       it("can't create a pool with no tokens", async function () {
-        await expect(this.gAMM.Init(1, 0))
+        await expect(this.gAMM.init(1, 0))
           .to.be.revertedWith("Can't create empty pool");
 
-        await expect(this.gAMM.Init(0, 1))
+        await expect(this.gAMM.init(0, 1))
           .to.be.revertedWith("Can't create empty pool");
       })
 
       it("recomputes K", async function () {
-        await expect(this.gAMM.Init(parseEther("7"), parseEther("191")))
+        await expect(this.gAMM.init(parseEther("7"), parseEther("191")))
           .to.emit(this.gAMM, "KChanged").withArgs(parseEther("1337").mul(WAD));
       })
 
       it("emits LP tokens", async function () {
-        await expect(() => this.gAMM.Init(parseEther("7"), parseEther("191")))
+        await expect(() => this.gAMM.init(parseEther("7"), parseEther("191")))
           .to.changeTokenBalance(this.gAMM, this.admin, parseEther("1337").mul(WAD));
 
         expect(await this.gAMM.totalSupply())
@@ -104,28 +104,28 @@ describe("Unit tests", function () {
       })
     });
 
-    describe("Mint", function () {
+    describe("mint", function () {
       beforeEach(async function () {
         // 1:5
-        await this.gAMM.connect(this.admin).Init(parseEther("1"), parseEther("5"));
+        await this.gAMM.connect(this.admin).init(parseEther("1"), parseEther("5"));
         // k == 5
         // total supply == 5
       });
 
       it("can't unbalance", async function () {
-        await expect(this.gAMM.Mint(1, 10))
+        await expect(this.gAMM.mint(1, 10))
           .to.be.revertedWith("Unbalancing");
-        await expect(this.gAMM.Mint(5, 1))
+        await expect(this.gAMM.mint(5, 1))
           .to.be.revertedWith("Unbalancing");
       })
 
       it("recomputes K", async function () {
-        await expect(this.gAMM.Mint(parseEther("2"), parseEther("10")))
+        await expect(this.gAMM.mint(parseEther("2"), parseEther("10")))
           .to.emit(this.gAMM, "KChanged").withArgs(parseEther("45").mul(WAD));
       })
 
       it("emits LP tokens", async function () {
-        await expect(() => this.gAMM.Mint(parseEther("2"), parseEther("10")))
+        await expect(() => this.gAMM.mint(parseEther("2"), parseEther("10")))
           .to.changeTokenBalance(this.gAMM, this.user1, parseEther("10").mul(WAD)); // double the original amount
 
         expect(await this.gAMM.totalSupply())
@@ -133,30 +133,30 @@ describe("Unit tests", function () {
       })
     });
 
-    describe("Burn", function () {
+    describe("burn", function () {
       beforeEach(async function () {
         // 1:5
-        await this.gAMM.connect(this.admin).Init(parseEther("1"), parseEther("5"));
-        await this.gAMM.Mint(parseEther("1"), parseEther("5"));
+        await this.gAMM.connect(this.admin).init(parseEther("1"), parseEther("5"));
+        await this.gAMM.mint(parseEther("1"), parseEther("5"));
         // k == 20
         // total supply == 10
       });
 
       it("can't unbalance", async function () {
-        await expect(this.gAMM.Burn(1, 10))
+        await expect(this.gAMM.burn(1, 10))
           .to.be.revertedWith("Unbalancing");
-        await expect(this.gAMM.Burn(5, 1))
+        await expect(this.gAMM.burn(5, 1))
           .to.be.revertedWith("Unbalancing");
       })
 
       it("recomputes K", async function () {
-        await expect(this.gAMM.Burn(parseEther("1"), parseEther("5")))
+        await expect(this.gAMM.burn(parseEther("1"), parseEther("5")))
           .to.emit(this.gAMM, "KChanged").withArgs(parseEther("5").mul(WAD));
       })
 
       it("burns LP tokens", async function () {
         await this.gAMM.approve(this.gAMM.address, parseEther("5"))
-        await expect(() => this.gAMM.Burn(parseEther("1"), parseEther("5")))
+        await expect(() => this.gAMM.burn(parseEther("1"), parseEther("5")))
           .to.changeTokenBalance(this.gAMM, this.user1, "-" + parseEther("5").mul(WAD).toString()); // double the original amount
 
         expect(await this.gAMM.totalSupply())
@@ -164,17 +164,17 @@ describe("Unit tests", function () {
       })
     });
 
-    describe("SellX", function () {
+    describe("sellX", function () {
       const xSupply = parseEther("10");
       const ySupply = parseEther("50");
       beforeEach(async function () {
         // 1:5
-        await this.gAMM.connect(this.admin).Init(xSupply, ySupply);
+        await this.gAMM.connect(this.admin).init(xSupply, ySupply);
         // k == 500
       });
 
       it("selling 0 tokens is allowed", async function () {
-        await expect(() => this.gAMM.SellX(0))
+        await expect(() => this.gAMM.sellX(0))
           .to.changeTokenBalance(this.xToken, this.user1, 0)
       })
 
@@ -183,7 +183,7 @@ describe("Unit tests", function () {
 
         // k == 500
         // with 10 + 15 = 25 X tokens, the AMM only needs to keep 20 Y tokens => 30 tokens are returned
-        await expect(() => this.gAMM.SellX(parseEther("15")))
+        await expect(() => this.gAMM.sellX(parseEther("15")))
           .to.changeTokenBalance(this.xToken, this.user1, "-" + parseEther("15").toString());
 
         expect((await this.yToken.balanceOf(this.user1.address)).sub(yBalance))
@@ -191,22 +191,22 @@ describe("Unit tests", function () {
       })
 
       it("emits event", async function () {
-        await expect(this.gAMM.SellX(parseEther("15")))
+        await expect(this.gAMM.sellX(parseEther("15")))
           .to.emit(this.gAMM, "SoldX").withArgs(parseEther("15"), parseEther("30"));
       })
     });
 
-    describe("SellY", function () {
+    describe("sellY", function () {
       const xSupply = parseEther("50");
       const ySupply = parseEther("10");
       beforeEach(async function () {
         // 5:1
-        await this.gAMM.connect(this.admin).Init(xSupply, ySupply);
+        await this.gAMM.connect(this.admin).init(xSupply, ySupply);
         // k == 500
       });
 
       it("selling 0 tokens is allowed", async function () {
-        await expect(() => this.gAMM.SellY(0))
+        await expect(() => this.gAMM.sellY(0))
           .to.changeTokenBalance(this.xToken, this.user1, 0)
       })
 
@@ -215,7 +215,7 @@ describe("Unit tests", function () {
 
         // k == 500
         // with 10 + 15 = 25 Y tokens, the AMM only needs to keep 20 X tokens => 30 tokens are returned
-        await expect(() => this.gAMM.SellY(parseEther("15")))
+        await expect(() => this.gAMM.sellY(parseEther("15")))
           .to.changeTokenBalance(this.yToken, this.user1, "-" + parseEther("15").toString());
 
         expect((await this.xToken.balanceOf(this.user1.address)).sub(xBalance))
@@ -223,7 +223,7 @@ describe("Unit tests", function () {
       })
 
       it("emits event", async function () {
-        await expect(this.gAMM.SellY(parseEther("15")))
+        await expect(this.gAMM.sellY(parseEther("15")))
           .to.emit(this.gAMM, "SoldY").withArgs(parseEther("30"), parseEther("15"));
       })
     });
